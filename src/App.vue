f50d0c0ec5d30f9019e8509ecbaded8e3f63f4a5
nblkmal <nblkmal@gmail.com>
2025-08-23 18:18:34 +0800

    chore: cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import GuestLayout from './components/GuestLayout.vue';
+import { Analytics } from '@vercel/analytics/next';
 </script>
 
 <template>
-  <GuestLayout headTitle="Kiratech">
-    <RouterView />
-  </GuestLayout>
+    <GuestLayout headTitle="Kiratech">
+        <RouterView />
+        <Analytics />
+    </GuestLayout>
 </template>
